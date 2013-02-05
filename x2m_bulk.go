52cea59f43499364f11a353d6d508a5109819e7b
clbanning <clbanning@gmail.com>
2013-02-05 06:28:19 -0600

    Comment cleanup.

--- x2m_bulk.go
+++ x2m_bulk.go
@@ -1,6 +1,3 @@
-// Copyright 2012-2013 Charles Banning. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file
 /*	
 	Process files with multiple XML messages.
 
